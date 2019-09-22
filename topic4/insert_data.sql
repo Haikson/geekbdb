f411dc355c4bed5c4698777fac574c7871940a3d
Kamo Petrosyan <kamo@haikson.com>
2019-09-22 20:04:04 +0300

    new file:   topic4/insert_data.sql 	new file:   topic4/structure.sql

---
+++ topic4/insert_data.sql
@@ -0,0 +1,71 @@
+
+/*Data for the table `communities` */
+
+insert  into `communities`(`id`,`name`,`private`,`logo`) values (1,'Ğ–ĞµĞ½Ğ¡Ğ¾Ğ²ĞµÑ‚',0,'/akndawd.gif');
+
+/*Data for the table `communities_users` */
+
+insert  into `communities_users`(`community_id`,`user_id`,`created_at`) values (1,1,'2019-09-22 16:32:03');
+insert  into `communities_users`(`community_id`,`user_id`,`created_at`) values (1,2,'2019-09-22 16:32:05');
+
+/*Data for the table `emoji` */
+
+insert  into `emoji`(`id`,`name`,`file`) values (1,')','GIF89a2\02\0÷\0\0\n#9?#-*\n-&2,$5#4%:)++\'-665-$<.$3*\':4*98602-!<J@W2=E<A=EZ)DL(FY;EH9KU>T[*Ng+Sm(Vv7Vj5\\x6Pd IgE3%I9-C<4B>:I8.E5IE<UH;\\P9cS9IIGLRNLW[VNHZTLTTT\\[WKPTF\\iE_rU^cIfyZejXlx\\s{Lcjg[Ld]Xef^mh^ti\\dedhkifnskvyunfxsivzwqqrIlƒ\\u„\\}Œ[yˆY~—Pq‰e~‹i{‡e~“v…~‚}^ƒš`Œ^¦dƒk„Œe†–fŠl„•l…›lŠ–md†š|†‹y…‰ux‹–{•e£l£eŠ¦f“­m’§m•­h”¨m–³n›µjœ¶u¤t–¬uš¯{“¦|›­w–¨t—´tœ´u»|´|¹u¤¾|¥¹o¢µk È|«Äz©Æz¯Ñƒzm‡…{„ˆŠ‡ƒŒ•”–‰–œ™––š™“‰ƒœ­‡™¦„µ™¤‹£ª„¤¶†ª½‹¤µ¬½†§»š¥ª”®½—ªµœ³½§¬«¥¯µ¨³¸¹¶«³¼½¶¹·°°ª § „¬Åƒ®ËŒ¯Ä‡¦Ã²ÆŒ³Í‡¶ËŠ¸Ö–°Ã•³Å“·Ì›µÄœ½Î™ºË”¼Ô“¿Ü›¾Óš½Ø§¼È£¿Ó³¿È½á¹Á½ÄÜ™ÂÚÂÕ›ÉêªÂÌ¥ÃÕ¤ÆÜ­ÆÔ«ÆÛ­Ëİ«ËÚ´ÅÎ½ÃÅºÉË´ËÖµÍÜ¼Íİ¹ÊÔ¼ÓŞ¹ÒÜ«ÑÜ¬Îá¦Êä¬ÒäªÔé³Îä½Öä¸×éºİñÃÃ»ÇÊÉÅÏÓÍÔÖÆÖÛÖİİÚÖËÅÚåÅŞíÈÚçÖßãÜâßÍäîÌáéÊæöÔæíİëîØåéÖëõÔìüİíõÛïû×çóŞóıÜò÷äåÜäííéìêãîôåïøêòğåòõäôûêòõéøşöûı\0\0\0\0\0\0!ş%Created by fCoder Graphics Processor\0\0!ù\0\0\0\0,\0\0\0\02\02\0ÿ\0€àpA D„X¸pDˆ$FH,±„$2–ØH±Æ‹ C€	buŞèÑˆËS«b®¢EkZ¯\\ÃŒY«V\r˜O`Ó¦Æ¬˜-[µZÕJš´•)T–0á!a™5züÄáÃgP¡S§L­jE–Ö­bhm1³f¬2dºDÃ•ç-¥KkÑZÕÒ&Â|YãfÏÖA`Çlµ¨­ºÅ¬¡-×¨dÌŠê«q-_¾öªRuI‡*P HùÂæa9{¸úT¨ĞX\\º¸uÅÑ¢G‰‘Ê®Yİ^·h)\n&¸@‰%Kë6mâhçÊG T°ˆÿû4)ÉŒ8b¨¨±¤J¤\\ÍØÚì…\\/,X³b=ÀE:/Y¨¡ÒJ{dåGV’Äâ‰#KÔ BÄ€Ã\rF,±Cf|ÒL3C\r3L/¶Èâ‹‡ÇpÁ_Zd¡¢ul°!…-¾áÇ%‘0!D)Hƒ‰x²Í£\\Ñ#ÍŒƒ5Öóa/Æ`“Í3\\lÀ…bTéÅ\ZcŒ†aĞqÇŠ\\¡„B¬\0*$‰&Îè¢9å|3‰º¬33Ä,Í5Ù Ó§9T€…„jñŸ^h	a„qÄ6Ğ°‚™+ØĞ#‘€nİÔÃ\r\"šxO3á\\ƒÎ©¨šÓ…Zx!F«\\œÿ‘Iø°Ã…MQÃ\n\'œÀ@ì OXaE$›xòM=d¸Î7é¨Ã;ì°cÎµ]¡ÛrÑ?ø€ç9 Â\n\n  @€\ZwĞ…OÜAŠ#£|’(êtóN>úèµ×š±A•U¦!\ZIô C-´p½¦Û.XA‡N”ÀAœŒrÄ4áH§ùì£r>ê¬³N:fXP(büğ=Œ|CHÀÀÄ\0\0=;…VLql2M Q²#ßô£²ÊúÈ£ÎÕhhås‘\"ˆ`¡D0”A	PÆNğ‡‚HÉ$“3Š#º@½O=øôÿİ7=€»S†_d)ÆIü€CıÂ,¬Á\r``ƒO¬d!‘X¡H³w\"ÛÔc:>€§NÏ:fh\0GxäQs]ìpÃ5¸€$@^aÅJzHb¼\"Š,Ò©:Lx’L?ø¨x>ô´ÜºyØG\ZUtÑğ7¤\0¼p¶\rMxn|*©t|ŸÓ\rŸtxôSSOÎ%lŸÆÿUHBd‚	œiX@\0°€ÂŠX	û$á¹I,bßĞ…Ó¸ñuĞ# ÔG>Ü±?ä!cøŸ÷lĞ	<`P\0Ù	ÁUX_*¬ğ$\\á‹Ø†#ÿ® ‰dt°zôp‡=ÜÑ²+l€”`Ä\"±„#Ğ H€ÒU\0À°ŠFãÑÁs‘E(¶a†F|\"äX‡´¤õw´Ceğ\0%:A‰E”a›€p¬«\0\'X\0@„[İa‚V€D$¡ytC	H7¸1u˜ì¨ãËğG”L8ÄN »^±àgêb\0»”€‹¸œ\'BR(aà(áŒk˜íh:`æ\0,œG7¨A4ƒ ;İ‘ïX×Š€‹u¸Ã±>ÑˆF¤2ˆ@Æ\'8Ñ	b<UèP\Z ƒ#È 7(B‚ÀÿÏ ¼€`\0\0%PÀ˜’ÄâÅ4<\"J¨AJÑ	N@‚–…4²ÁÑs˜Á=èÑ!¡OÁ0Ø\"ùTz‚aIˆ&\\!ND‚¸0„†0GhBÁ D%†ŠŠYC\ZÁğÌğ1áqA0„!‚‚< \rHÀ\\˜‚ \"È°6–á\nd|ã.PÂ&¡‰Ml˜\0D\0a	UØ>p€#\Zaƒ\ZÔ€Ÿûô\'±*ßiµ‹.páŠb\\ãé`‡!Zp,h‚ÁàÄ&^¡Š¾Ôõ9pÀ\"Ì ƒ!,ÁKHm$ ¶P p­” ŠdÿlêÁx†9ø¡G´`†2ˆXüD°èK,xĞD	‡h„T©Ù‚N 1ğ@=™àO(ãË(•5ö¤|„â·¤€†2‚aDƒ\ZÑè,F³	 Ì€H@pR[!‚¼°2@ñ	^l£î3øe¬CÚ¸x¡Œg@ÕpR5¢q_ÈâÌUD\"Ìp#ìs	¡0‚Ÿ¥ht£å¨9ÔÙvÈÃËğ†:æñˆRøÏPO²\rjCÂ˜B°ğCa@1tadŠcCÏp4Â¡%2qì¨ÚËÔArtCê]Æ2ò$#ËâÿÄøAFz„L@‹èD,r\rqX£ºX¬3Â}ÜãĞÕ»ÚÕäByĞãŞp2ÖLŒcH#\ZÆ€ó&À„!´€ $6q‹\\ ©.›˜Å2®!wèÃüp‡;&Z3šü°Ú9Jµæe0ãÖ`Æ2Ğ\0OPŠø$$ñŠR#®ØD.ÔsÜ#`úøWÕ¢Uk–ÉƒZì€ÇcŸ¡§k\\Ã\ZUH€Zˆ1â}Ä)–r‹ûÌâ(°às;â:¾ƒ‘\r³>\0ğ€ÃÃáXğ8®‘nÑà7Ğ„&ì`‡A´â.£y*\0aH \"Ì •9Ş1°s\\£Zÿ›õÀrÍu„ÃŞ`øRÊFY)YZ1@BjPàP‡>¤âÄÈF2¯áÎkœcÁæˆ?Ho°ãöF°!á˜zƒ)NQ§¼ápÈÜ †ÁT\"Ş®`†8üŒñÄ¢ËˆúÀ‚ñ‰tœÚº„Pƒ4á—ˆÃæPV´‚p˜Ã¶09´aèjXCÑ1!k\\ã°j,s¼CÏ@C0Òñ–fP!„ow6Á‰AtGç¿ø…$ß†?ìArxa4‰ÆZC¯ÈD%6ñ\ZhÀÅß_…	Á*ÀÁp±‰6l!ß°ÿ%ÿBâü–‡CĞ5_	LôÁ\'¬„*d1ŒXC\ZÀÅ5Ö+D ¯1pW@›`y„~¿P~„P~{ïguPty\0…³x°¯°Ì\'l«f\r`7`HPÂP€m@€ğÔ\0\n”Àr x½÷s¸}°ƒxğ~k@YàDg¨`¶@\rÔ`ÆÀQpP_çW˜0y‹\'ÔĞ	Á\0q€m08øv ƒ}àn°Aç}P	™\0Â`Ù`9a\rQ0G3=À™\0	B÷j0”p\nÚ±^x~¿À\n€gïÖ\')Qr\0€@½ \n¦\r½p\\Ø@pFĞR	i [Ğj™\0ˆ\0‚„\0|\0†8ˆm÷*¢j˜	«\0rP\r´ \n¿°Qe‰óf\\à?ĞhÀ	ŒpIğ\nœ P}`	Í¡\n–0TX	Û#t0k\0•ğ\nĞ°	”\0\nÂ0¨`U@\0ğ°3\0°à3Ğ0ĞO0< <ÀSpNP99ğ:\0N0P@@ğ@ğ>POà\0;');
+insert  into `emoji`(`id`,`name`,`file`) values (2,'(((','GIF89a2\02\0÷\0\0\0\0\0\0$\0,)\n5=;81;)DLKJLUT\r\\]XW]\0[\0WZMU\'[)\Z\\8Q.edk\nk\rdi	fu|}~\rw\nwy}\rh\'k)g9x\'	y\'{4}4	z9*p61a\Z#tDuJ2\\A\nyXMmjK\r…\n…Š	‡\Z••\0›\r™\r\n•œœ›–›„\rŠ\'	†*š\'˜$—5”7Š+)Š;)–60¦¤¥­¬©ª·µ¼¼¼·\Z¹«&¦&\Z¸&¹(°6±4.!M‹D(‹Z9œJ(–K6•V>“W/»Y\Z¬G¨I)ªU+©Y7¶P2³j9d:TJ–jV±WL«hV¹xV±lI­wj—tbÍÇ\rÆ\0Ì\0ÉÔ\0ØÍÉ&Ğ2Ë;(Ò9-â#\"ÉLĞP1Æi%Ìw*Ôw)Ñr4åO9Óo&ÑRKÈiIĞvPÏtgë\\LïsRúskä]aµ‡W¼šy¶‘nŒ‰j×ˆ(Õˆ<Ô0â˜<Æ…YÙˆDİ˜JÕ[ÌŠlÊ™yÖ‡jÛ™hÓ‘tß¥mÙ¨{Ñ¶è•Zğ‰TíŠfé™yú™dõrë©Yí®Sè©yéµjû¨nî­và¥Wµ›·ªŒĞ™‘È©‰Ê§™Ø¬‡Õµ“Û¸¨Ù¶³ô”†ç·‰é»™ş©ú¶—ó¬‘ÿº¦ó¶¯ÕÉ®ôÇ™çÉ©çÊµıÇ«ûË·ı×ªüÙ¹è×½ÿçºßÂ˜ÚîæûÛÊûÚ×îÓÍûßåêæØşçÉıêØıøÙêüÚçêèí÷éìúøşêéşíöşõåşôíÿüî÷úè÷ÿÿôşüşõôşôışüõıüşõö÷ğëòÚãÎ\0\0\0\0\0\0\0\0\0\0\0\0!ş%Created by fCoder Graphics Processor\0\0!ù\0\0\0\0,\0\0\0\02\02\0ÿ\0Èè £à?|¨QCŠ”)B¬¸r…†‹!\\´É‚\"Šˆ!B´2£$2dĞÀXÉ’ÕJæL™-[ÎœE«åiP›4nÚ\0ò“%+{<‰js…È“\'R¨Xë×¬X«\rÚ:(¥hşr&ŒÖ/aÂ~Ú“¦I-K&BÄÉ#Mšü@ÂÅÉS%Aœ°Ù“«páN|ôÌ‰ñ¡-™¿hÑ*›‹•(y»liÒwˆ\'Mœ(Q‚É/O›<ùÅÉ<{öä±2%\n\rF™½uk×.\\”Ø0éâ…L˜%HŒ(ò#ˆóÀ¢IÑ¢¥ô’-K–(a²eKéÒ.(tÿ(Œ–¬`»@İaCšx˜êH~È¢¢ş|#N¸pÃ…Ki#É%g„EH‚x£¸\".À¡±Å^|¡ÅGhA	#dX@Â	$¸àP\0‘DO$Q\ZiHtÇÅ„E„—Û0²¬…Ù•6á„$¼€a†\"P@‚P	+@qbØDuLFy„d°J,œP2…r?,(¼\0EXp‚	\"X Á\\À`ôÂœ+Ô¹ÂvA\"‰eN È*z°1ÄKü\0D\n@±Âœ\\pÂ	\"T énrğ(	!yÄ˜(Ô§B\n+\0¡ç\n}\rC¨à\\!T`Âpÿ°Á1ÄP’I,Ağ@%€)&)X&¥ÈÆŠÁRˆà)\n#´ph€Ã„Èrt+ÇtøqC\r.´Ğ‚\"œ9ºÄvHd¬±&`A\r&ğŠ®	0Ü°ƒô@3âŒãÌ+ƒğ!È(È(Ã2¯ü!E	%ğZ‚	OğÀ	4`À`@\rl1F-`AG …øñ2âœÓ<ædC‹‚Ñ˜sÎ9áh#Í 3”\0¬7`…\r4pÀ tÒh€Á˜\0G ¥ÀB)ÅŒã2;óÄóÎ5Ç\\ƒÎ=÷ä£9â ã°ztâI,‚À¡k(\r\0\0I# ÁˆÿPC ¯ãÌ2Ê`cÎ;ïà3<ïps\r7èÈs>ùÀ5„ä1ˆ.Ùt. 1x—nÀ¸¹¥£L5ØŒ3;öÔn>öè£O>·÷^»:Ä”rÍÍ7ƒÌs@’ÆIôG,ÀA1BÌ1Õt³°8´×Ï÷·/>îö´ÃŒÖˆ#3\0ÒFZpÆT €Ç\0,àC1ÈCŒ=G=ÀAx0òxÇx”Crd«G=Ú¡flb\rÅ	È@…\0Àn`€LaŠ(\0;xº-Œà©ˆ,p¡8ÆÃóì\0€€\0ÿpB„#é¨‡=ê|LnŠ£Ç<¦(EÅA±vä FÀ†1$\"ˆ8D@FøĞ‡Bô\nQtèãTG>ğA(Jq\\›âiGc<Š8Ä!¾ˆˆJàQ@À\"àL`,@xàcPƒÆ`™•XÚ¹£KÌ‡Éájxb~‰0„*¿H Hà(.¸A ñq‰h\'\0Á<€ƒ^ö€eâ\0å\'?I{Üc“äh5Ü\02\"‹X+‚-‚aÍ˜Æ4´‹\Zh@\ns¨ ¬ ‡ËHÁ\nxèD3¢‘\rnhƒá°ÎÎAMPaƒ‹`D*‘ÿˆVVÓš¢ †8ÌÁWÄa±Ğ…4‚‘LÈrÀÃä\0‡ŠÎá5‹g8hH…{š!šĞD\"Ò@M=DâZh\' Á\ro(c°x†6¸±müµ¨,`QµBŒ¢¥8†PIAŠc”ÂRˆBºF‘nğ	ÔÄC$NÊ)àßøÆ1Á\rs„Ã`µFMÁjolã\ZæH+7Öz\rmLã¼ÈÂREzDa­Ä@ A	I8âAUà„.¢ÑUt¼CñHG9Ê±Ä®#ê(‡:Ö!Åw0î7Ë†˜0…2ˆacØ`K ‡I´ÁPÃ\Z¢P@èBçˆGäÖQ}ÿ<–ld›,éÁâyíÓD\Z¦ †ÏAƒ„2¡C0â¹ŒXÄ2amÈ±ñX¬áQ{´câ¸‡1ï1upíúX!ö`…êˆ!]à`\n0°\nPL\"ŸÏ•#\Zq†,xbôxÇbËñcÂñ» Ü¤:À1z¬C×@\'¨À¤1¼7O@A\nLÜachD&$ñG¨¡”à…;Û™wDîİGääá`ğZ³ÌXÆ1p 4AF@ÃRQtxl8n#$±äÔª±hFáĞ1ctt£‘‹‡ƒf¹ïV“;p€Tà& \Z^+X¡‰I¬AÿaC1‰L@Âdˆ„( s¨U\'…µò1¶at˜ãËÀ)vP\0< B“Â¬´æKhâñÍá#@Š5œ”\nm˜Å4<7gdƒÊæ¨)Qhn(ãu¥A\0p€Has }YŠK\\\"8ØQÃÔ‰P„\"”X\ZŞG‰Md\"Ÿà4¢\r^°\"b†,fñ¹­Ñ¸b°ë^[šj¸\ZĞğ=C#”HCG§€†5¨áuE–ò€	Ş¬\"1‚èCˆ†€  7}QŠSœâ•˜D¶ƒ$<¡\r”ÀE3xÁ‰6¬a\rbPhç\\5Üû(ÿ7ñÇ¿0(¡B@	<ŠV8âv@r” äÕT¡\r €4>†9;e(ƒÔ°t¦—¼fğ@\"PA\n-(x^ÑŠV¤â¨°D%ìp‡‰á)_øB\Z ±‰Pğ¢Èf:\Zæ†1ˆ”Ÿª4Ä!Ú¥öçÀÀ+†Ñ\nU¤\"ÆÙ§ %)!;òƒD$œ	£aİS¸{ŞaEÂ8ñí°õaøBª`8+ÄNö*Ğ§KOù#ÀĞG8\"éJWD„ÌĞ…âWd(îR¯sÒÏ¢¦WE+PÁ\nL°¾ìA@ÔP4ûê‹Á \r­¼°Ô÷¾´ÂïÎÿ6ğ€ÀÀğ…/º¾f±½ì0€X	^à\"8…\nÛ~#Ñ4ä…	8’Ú¡e&\ZÅ—ç—~¾Ğ¼Á\nw\nc—o`õ,$Ÿ±\\g‡Ğ ÁM\0€×#L°¢!Ğ\0æ‡~êè±†çp|`	&08¸7NSæâ£Q‚Û„Øxz ‚É€~éç‚èQxwxœo@%`1\rğ\0€-0ÎA\Z£ñ…£Á†)° «f\rÎ`\rjHe!3cd£P0u°/££\0 \0\n\0,1+ò(&ğ(\" ±°\0°q2Ğ}P’XoP‰o •¨:P00:C\0´v\0W(f rŠ f\r€\0\0I\0;');
+
+/*Data for the table `friendship` */
+
+/*Data for the table `friendship_statuses` */
+
+insert  into `friendship_statuses`(`id`,`name`) values (2,'accepted');
+insert  into `friendship_statuses`(`id`,`name`) values (3,'rejected');
+insert  into `friendship_statuses`(`id`,`name`) values (4,'removed');
+insert  into `friendship_statuses`(`id`,`name`) values (1,'requested');
+
+/*Data for the table `likes` */
+
+/*Data for the table `media` */
+
+insert  into `media`(`id`,`media_type_id`,`user_id`,`filename`,`size`,`metadata`,`created_at`,`updated_at`,`private`) values (101,1,101,'/uploads/gjgfdskfb8976542brbcsd.jpg',1024,'{\r\n    \'some_data\': \'some_value\'\r\n}','2019-09-22 16:54:41','2019-09-22 16:54:41',1);
+
+/*Data for the table `media_types` */
+
+insert  into `media_types`(`id`,`name`) values (6,'audio');
+insert  into `media_types`(`id`,`name`) values (8,'document');
+insert  into `media_types`(`id`,`name`) values (5,'image');
+insert  into `media_types`(`id`,`name`) values (7,'video');
+
+/*Data for the table `messages` */
+
+insert  into `messages`(`id`,`from_user_id`,`to_user_id`,`body`,`important`,`created_at`,`status`) values (101,103,104,'ĞŸÑ€Ğ¸Ğ²ĞµÑ‚.\r\nĞšĞ°Ğº Ñƒ Ğ²Ğ°Ñ Ğ¿Ğ¾Ğ³Ğ¾Ğ´Ğ°?',1,'2019-09-22 16:57:10','2');
+insert  into `messages`(`id`,`from_user_id`,`to_user_id`,`body`,`important`,`created_at`,`status`) values (102,104,103,'Ğ—Ğ¸Ğ¼Ğ½ĞµĞµ ÑĞ¾Ğ»Ğ½Ñ†Ğµ ÑĞ¾Ğ³Ñ€ĞµĞ»Ğ¾ Ğ²Ğ¾Ğ·Ğ´ÑƒÑ… Ğ´Ğ¾ 20 Ğ³Ñ€Ğ°Ğ´ÑƒÑĞ¾Ğ². ĞĞµĞ½Ğ°Ğ²Ğ¸Ğ¶Ñƒ, ĞºĞ¾Ğ³Ğ´Ğ° Ñ‚Ğ°Ğº Ñ…Ğ¾Ğ»Ğ¾Ğ´Ğ½Ğ¾ Ğ¸ Ğ¼ĞµÑ€Ğ·ĞºĞ¾.',0,'2019-09-22 16:58:29','1');
+
+/*Data for the table `posts` */
+
+/*Data for the table `profiles` */
+
+insert  into `profiles`(`user_id`,`sex`,`birthday`,`photo_id`,`region_id`) values (101,'M','2019-09-12',1,102);
+insert  into `profiles`(`user_id`,`sex`,`birthday`,`photo_id`,`region_id`) values (102,'M','1946-01-01',2,103);
+insert  into `profiles`(`user_id`,`sex`,`birthday`,`photo_id`,`region_id`) values (103,'M','1689-12-01',3,104);
+
+/*Data for the table `regions` */
+
+insert  into `regions`(`id`,`name`,`parent_id`,`code`,`zip`,`created_at`,`updated_at`) values (101,'Ğ Ğ¾ÑÑĞ¸Ñ',NULL,7,NULL,'2019-09-22 19:23:46','2019-09-22 19:23:50');
+insert  into `regions`(`id`,`name`,`parent_id`,`code`,`zip`,`created_at`,`updated_at`) values (102,'ĞœĞ¾ÑĞºĞ²Ğ°',101,755,125000,'2019-09-22 16:24:23','2019-09-22 16:24:23');
+insert  into `regions`(`id`,`name`,`parent_id`,`code`,`zip`,`created_at`,`updated_at`) values (103,'Ğ ÑĞ·Ğ°Ğ½ÑŒ',101,160,122552,'2019-09-22 16:24:52','2019-09-22 16:24:52');
+insert  into `regions`(`id`,`name`,`parent_id`,`code`,`zip`,`created_at`,`updated_at`) values (104,'ĞšÑ€Ğ°ÑĞ½Ğ¾Ğ´Ğ°Ñ€',101,23,116000,'2019-09-22 16:25:27','2019-09-22 16:25:27');
+
+/*Data for the table `target_types` */
+
+insert  into `target_types`(`id`,`name`,`created_at`) values (1,'media','2019-09-22 15:59:31');
+insert  into `target_types`(`id`,`name`,`created_at`) values (2,'user','2019-09-22 15:59:31');
+insert  into `target_types`(`id`,`name`,`created_at`) values (3,'community','2019-09-22 15:59:31');
+insert  into `target_types`(`id`,`name`,`created_at`) values (4,'message','2019-09-22 15:59:31');
+insert  into `target_types`(`id`,`name`,`created_at`) values (5,'post','2019-09-22 15:59:31');
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`first_name`,`email`,`last_name`) values (101,'Ğ›ĞµĞ²','l.tolstoy@yandex.ru','Ğ¢Ğ¾Ğ»ÑÑ‚Ğ¾Ğ¹');
+insert  into `users`(`id`,`first_name`,`email`,`last_name`) values (102,'Ğ¡Ñ‚ĞµĞ¿ĞºĞ°','razin@mail.ru','Ğ Ğ°Ğ·Ğ¸Ğ½');
+insert  into `users`(`id`,`first_name`,`email`,`last_name`) values (103,'ĞœĞ¸Ñ…Ğ°Ğ¸Ğ»','upyr1946@mail.ru','ĞŸÑ€Ğ¸Ğ³Ğ¾Ñ€Ğ¾Ğ´Ğ½Ğ¸Ğ¹');
+insert  into `users`(`id`,`first_name`,`email`,`last_name`) values (104,'Ğ¡Ğ²ÑÑ‚Ğ¾ÑĞ»Ğ°Ğ²','technoboy@gmail.com','ĞŸÑĞºĞ¾Ğ²ÑĞºĞ¾-Ğ’Ğ¾Ğ»Ğ¾Ğ³Ğ¾Ğ´ÑĞºĞ¸Ğ¹');
---
+++ topic4/structure.sql
@@ -0,0 +1,210 @@
+/*
+SQLyog Professional v12.5.1 (64 bit)
+MySQL - 8.0.17 : Database - VK_EXAMPLE
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `communities` */
+
+DROP TABLE IF EXISTS `communities`;
+
+CREATE TABLE `communities` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `private` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `logo` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `communities_users` */
+
+DROP TABLE IF EXISTS `communities_users`;
+
+CREATE TABLE `communities_users` (
+  `community_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`community_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `emoji` */
+
+DROP TABLE IF EXISTS `emoji`;
+
+CREATE TABLE `emoji` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `file` mediumblob NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `friendship` */
+
+DROP TABLE IF EXISTS `friendship`;
+
+CREATE TABLE `friendship` (
+  `user_id` int(10) unsigned NOT NULL,
+  `friend_id` int(10) unsigned NOT NULL,
+  `status_id` int(10) unsigned NOT NULL,
+  `requested_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `confirmed_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`friend_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `friendship_statuses` */
+
+DROP TABLE IF EXISTS `friendship_statuses`;
+
+CREATE TABLE `friendship_statuses` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `likes` */
+
+DROP TABLE IF EXISTS `likes`;
+
+CREATE TABLE `likes` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `desc_id` int(11) NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `value` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id_fk` (`user_id`),
+  CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `media` */
+
+DROP TABLE IF EXISTS `media`;
+
+CREATE TABLE `media` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `media_type_id` int(10) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `filename` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `size` int(11) NOT NULL,
+  `metadata` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `private` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `user_id_fk_media` (`user_id`),
+  CONSTRAINT `user_id_fk_media` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `media_types` */
+
+DROP TABLE IF EXISTS `media_types`;
+
+CREATE TABLE `media_types` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `messages` */
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(10) unsigned NOT NULL,
+  `to_user_id` int(10) unsigned NOT NULL,
+  `body` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `important` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `status` enum('0','1','2') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `from_user_id` int(10) unsigned NOT NULL,
+  `to_communitie_id` int(10) unsigned NOT NULL,
+  `body` text NOT NULL,
+  `important` tinyint(1) DEFAULT NULL,
+  `delivered` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `profiles` */
+
+DROP TABLE IF EXISTS `profiles`;
+
+CREATE TABLE `profiles` (
+  `user_id` int(10) unsigned NOT NULL,
+  `sex` char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `photo_id` int(10) unsigned NOT NULL,
+  `region_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `profile_region_id_fk` (`region_id`),
+  CONSTRAINT `profile_region_id_fk` FOREIGN KEY (`region_id`) REFERENCES `regions` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `regions` */
+
+DROP TABLE IF EXISTS `regions`;
+
+CREATE TABLE `regions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `parent_id` int(10) unsigned DEFAULT NULL,
+  `code` int(11) DEFAULT NULL,
+  `zip` int(10) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `region_parent_fk` (`parent_id`),
+  CONSTRAINT `region_parent_fk` FOREIGN KEY (`parent_id`) REFERENCES `regions` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `target_types` */
+
+DROP TABLE IF EXISTS `target_types`;
+
+CREATE TABLE `target_types` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`first_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
