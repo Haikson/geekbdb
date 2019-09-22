4dcc6b14da907af1c42eda39720533640de6e068
Kamo Petrosyan <kamo@haikson.com>
2019-09-22 20:42:04 +0300

    modified:   structure.sql

--- topic4/structure.sql
+++ topic4/structure.sql
@@ -1,18 +1,4 @@
-/*
-SQLyog Professional v12.5.1 (64 bit)
-MySQL - 8.0.17 : Database - VK_EXAMPLE
-*********************************************************************
-*/
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `communities` */
 
 DROP TABLE IF EXISTS `communities`;
 
@@ -204,7 +190,3 @@ CREATE TABLE `users` (
   UNIQUE KEY `name` (`first_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
