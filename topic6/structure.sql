f1f170103991a3a827c141c7dfdd87138a2d8d9c
Kamo Petrosyan <kamo@haikson.com>
2019-09-29 00:55:05 +0300

    modified:   topic6/structure.sql

--- topic6/structure.sql
+++ topic6/structure.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE regions (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  name varchar(150) NOT NULL,
   parent_id int(10) unsigned DEFAULT NULL,
   code int(11) DEFAULT NULL,
   zip int(10) DEFAULT NULL,
@@ -14,9 +14,9 @@ CREATE TABLE regions (
 
 CREATE TABLE users (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  first_name varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  email varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  last_name varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  first_name varchar(100) NOT NULL,
+  email varchar(150) DEFAULT NULL,
+  last_name varchar(100) DEFAULT NULL,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   birthday datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -26,9 +26,9 @@ CREATE TABLE users (
 
 CREATE TABLE communities (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  name varchar(150) NOT NULL,
   private tinyint(1) unsigned NOT NULL DEFAULT '0',
-  logo varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  logo varchar(255) DEFAULT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY name (name)
 );
@@ -42,7 +42,7 @@ CREATE TABLE communities_users (
 
 CREATE TABLE emoji (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  name varchar(150) NOT NULL,
   file mediumblob NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY name (name)
@@ -59,7 +59,7 @@ CREATE TABLE friendship (
 
 CREATE TABLE friendship_statuses (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(150) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  name varchar(150) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY name (name)
 );
@@ -79,9 +79,9 @@ CREATE TABLE media (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   media_type_id int(10) unsigned NOT NULL,
   user_id bigint(20) unsigned NOT NULL,
-  filename varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  filename varchar(255) NOT NULL,
   size int(11) NOT NULL,
-  metadata longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  metadata longtext,
   created_at datetime DEFAULT CURRENT_TIMESTAMP,
   updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   private tinyint(1) unsigned NOT NULL DEFAULT '0',
@@ -92,7 +92,7 @@ CREATE TABLE media (
 
 CREATE TABLE media_types (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  name varchar(255) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY name (name)
 );
@@ -101,10 +101,10 @@ CREATE TABLE messages (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   from_user_id int(10) unsigned NOT NULL,
   to_user_id int(10) unsigned NOT NULL,
-  body text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  body text NOT NULL,
   important tinyint(1) DEFAULT NULL,
   created_at datetime DEFAULT CURRENT_TIMESTAMP,
-  status enum('0','1','2') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  status enum('0','1','2') NOT NULL,
   PRIMARY KEY (id)
 );
 
@@ -112,7 +112,7 @@ CREATE TABLE posts (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
   from_user_id int(10) unsigned NOT NULL,
   to_communitie_id int(10) unsigned NOT NULL,
-  body text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  body text NOT NULL,
   important tinyint(1) DEFAULT NULL,
   delivered tinyint(1) DEFAULT NULL,
   created_at datetime DEFAULT CURRENT_TIMESTAMP,
@@ -122,7 +122,7 @@ CREATE TABLE posts (
 
 CREATE TABLE profiles (
   user_id int(10) unsigned NOT NULL,
-  sex char(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  sex char(1) NOT NULL,
   birthday date DEFAULT NULL,
   photo_id int(10) unsigned NOT NULL,
   region_id int(10) unsigned DEFAULT NULL,
@@ -140,7 +140,7 @@ CREATE TABLE storehouses_products (
 
 CREATE TABLE target_types (
   id int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  name varchar(255) NOT NULL,
   created_at datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   UNIQUE KEY name (name)
