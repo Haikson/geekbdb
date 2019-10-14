83e05703c09b02f1d12a289cbf6f6e3b08c34478
Kamo Petrosyan <kamo@haikson.com>
2019-10-14 13:19:56 +0300

    topic 11

--- dump--.sql
+++ dump--.sql
@@ -355,7 +355,7 @@ DROP TABLE IF EXISTS `regions`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `regions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(150) NOT NULL,
   `parent_id` int(10) unsigned DEFAULT NULL,
   `code` int(11) DEFAULT NULL,
   `zip` int(10) DEFAULT NULL,
@@ -445,20 +445,17 @@ SET character_set_client = @saved_cs_client;
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `last_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `birthday` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `name` (`first_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4794 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
@@ -470,36 +467,3 @@ INSERT INTO `users` VALUES (1,'Hayley','ahmad12@example.com','Bashirian','1971-0
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping routines for database 'VK_EXAMPLE'
---
-
---
--- Final view structure for view `username`
---
-
-/*!50001 DROP TABLE IF EXISTS `username`*/;
-/*!50001 DROP VIEW IF EXISTS `username`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
-/*!50001 VIEW `username` AS select `users`.`id` AS `id`,concat(`users`.`first_name`,' ',`users`.`last_name`) AS `CONCAT(first_name, ' ', last_name)` from `users` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-10-12  6:03:19
---
+++ topic11/main.sql
@@ -0,0 +1,53 @@
+/**
+ * Создайте таблицу logs типа Archive.
+ * Пусть при каждом создании записи в таблицах
+ * users, catalogs и products в таблицу logs помещается
+ * время и дата создания записи,
+ * название таблицы,
+ * идентификатор первичного ключа
+ * и содержимое поля name.
+ */
+
+CREATE TABLE logs (
+	table_name VARCHAR(255) NOT NULL,
+	dest_id BIGINT NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	logged_at DATETIME DEFAULT NOW()
+) ENGINE=ARCHIVE;
+
+CREATE PROCEDURE write_logs (
+	IN table_name VARCHAR(255),
+	IN dest_id INT,
+	IN name VARCHAR(255)
+)
+BEGIN
+  INSERT INTO logs (table_name, dest_id, name, logged_at)
+  VALUES (table_name, dest_id, name, NOW());
+END;
+
+DROP TRIGGER IF EXISTS write_users_log;
+
+CREATE TRIGGER write_users_log AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	CALL write_logs('users', NEW.id, CONCAT(NEW.first_name, ' ', NEW.last_name));
+END;
+
+INSERT INTO users (first_name, last_name, email, birthday) VALUES
+('Savely', 'Kramarov', 'save@gmail.com', '1934-10-13');
+
+SELECT * FROM logs; -- users	4797	Savely Kramarov	2019-10-14 09:22:37.0
+
+CREATE TRIGGER AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	CALL user_logs('catalogs', NEW.id, NEW.name)
+END;
+
+CREATE TRIGGER AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	CALL user_logs('products', NEW.id, NEW.name)
+END;
+
+В базе данных Redis подберите коллекцию для подсчета посещений с определенных IP-адресов.
